1c264b7c2aff330f1d46d706e3290783d8e243c7
Santiago Montironi <santiimontironi@gmail.com>
2026-05-06 21:09:04 -0300

    first commit

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors(
+    {
+        origin: process.env.FRONTEND_URL,
+        methods: ['GET', 'POST', 'PATCH', 'DELETE']
+    }
+));
+
+app.use(express.json());
